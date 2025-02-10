0e8fd30b5b66e1f923f223020613c0e7a296d6da
Rex <rex@rexslab.com>
2025-02-10 17:56:10 +0200

    Update win.ps1

--- win.ps1
+++ win.ps1
@@ -8,8 +8,8 @@ $name = "Rex's Lab Scripts 4 Windows"
 $workDir = "$env:TEMP/$name"
 
 $scriptUrls = @{
-    "voicemeeter-no-crackle"  = "https://raw.githubusercontent.com/ThatRex/voicemeeter-no-crackle/main/voicemeeter-no-crackle.bat";
-#   "windows-virtual-monitor" = "https://raw.githubusercontent.com/ThatRex/windows-virtual-monitor/main/windows-virtual-monitor.ps1";
+#   "voicemeeter-no-crackle"  = "https://raw.githubusercontent.com/ThatRex/voicemeeter-no-crackle/main/voicemeeter-no-crackle.bat";
+    "windows-virtual-monitor" = "https://raw.githubusercontent.com/ThatRex/windows-virtual-monitor/main/windows-virtual-monitor.ps1";
 }
 
 function DownloadAndExecuteScript([string]$url) {
